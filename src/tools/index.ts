a4ad90c78b661f6a48d36676acad4aa79d3913e7
Šota Giorgadze <giorgadze@inbox.lv>
2026-04-15 11:46:25 +0200

    feat: implement chat tools and GitHub issue creation functionality

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -6,6 +6,9 @@ import { wopeeDispatchAnalysis } from "./wopee_dispatch_analysis/index.js";
 import { wopeeCreateBlankSuite } from "./wopee_create_blank_suite/index.js";
 import { wopeeFetchAnalysisSuites } from "./wopee_fetch_analysis_suites/index.js";
 import { wopeeFetchExecutedTestCases } from "./wopee_fetch_executed_test_cases/index.js";
+import { wopeeSendChatMessage } from "./wopee_send_chat_message/index.js";
+import { wopeeReadChatHistory } from "./wopee_read_chat_history/index.js";
+import { wopeeCreateGithubIssue } from "./wopee_create_github_issue/index.js";
 
 export const TOOLS = [
   wopeeCreateBlankSuite,
@@ -18,4 +21,8 @@ export const TOOLS = [
   wopeeFetchArtifact,
   wopeeUpdateArtifact,
   wopeeGenerateArtifact,
+
+  wopeeSendChatMessage,
+  wopeeReadChatHistory,
+  wopeeCreateGithubIssue,
 ];
